762b17545f7752b51476303fdb49101bb6b29c21
Alexander Gruzintsev <0v3rt1r3d@gmail.com>
2022-08-06 16:53:46 +0400

    Add script for packing solution

---
+++ toncontest2/pack.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir solution
+cp func/1.fc solution/1.fc
+cp func/2.fc solution/2.fc
+cp func/3.fc solution/3.fc
+cp func/4.fc solution/4.fc
+cp func/5.fc solution/5.fc
+cp participant.json solution/participant.json
+
+tar -zcvf solution.zip solution && rm -rf solution
